febc2520b2ea851fe143ea8a3f9afdccf43f6cf4
unknown <mdrokibulhassan786@gmail.com>
2023-08-18 21:40:24 +0600

    setup css, js, fonts, aos, bootstrap

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+    <!-- BootStrap Js -->
+    <script src="<?php echo $url ?>assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Aos Animation -->
+    <script src="<?php echo $url ?>assets/aos/aos.js"></script>
+
+    <!-- Custom Js -->
+    <script src="<?php echo $url ?>assets/js/script.js?v=<?php echo time(); ?>"></script>
+</body>
+
+</html>
---
+++ components/header.php
@@ -0,0 +1,33 @@
+<?php
+$url = "http://localhost/near_by/";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- BootStrap CSS -->
+    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/bootstrap.min.css">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="<?php echo $url ?>assets/aos/aos.css">
+
+    <!-- Aos Online-->
+    <link rel="stylesheet" href="<?php echo $url ?>assets/fonts/all.min.css">
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/style.css?v=<?php echo time(); ?>">
+
+    <!-- Admin Css -->
+    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/admin_style.css?v=<?php echo time(); ?>">
+
+    <!-- Admin Responsive Css -->
+    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/admin_responsive.css?v=<?php echo time(); ?>">
+
+    <!-- Responsive Css -->
+    <link rel="stylesheet" href="<?php echo $url; ?>assets/css/responsive.css?v=<?php echo time(); ?>">
+
+</head>
+
+<body>
